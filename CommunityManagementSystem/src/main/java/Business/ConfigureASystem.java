6c8e7bb6931ae2d61d054df86d5f08154dd58d05
Sowmika461 <adepu.s@northeastern.edu>
2022-04-30 17:50:59 -0400

    commit

--- CommunityManagementSystem/src/main/java/Business/ConfigureASystem.java
+++ CommunityManagementSystem/src/main/java/Business/ConfigureASystem.java
@@ -17,13 +17,13 @@ public class ConfigureASystem {
     
     public static EcoSystem configure(){
         
-        EcoSystem system = EcoSystem.getInstance();
+       /* EcoSystem system = EcoSystem.getInstance();
         
         Employee employee = system.getEmployeeDirectory().createEmployee("System Head",0);
         
         UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
         
-        return system;
+        return system;*/
     }
     
 }
